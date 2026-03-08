672f6158c9537bc5d74473248ed8050113705fa3
Jelly-Ely <javelytejeda@gmail.com>
2026-03-08 14:01:34 -0700

    Added Crow routes and frontend API listings file

--- backend/server.cpp
+++ backend/server.cpp
@@ -10,6 +10,9 @@ namespace asio { typedef io_context io_service; }
 #include "Marketplace.h"
 #include "Item.h"
 
+#include <string>
+#include <vector>
+#include <iostream>
 /** Returns true if user was created, false if username already exists. */
 bool createUser(Userbase& userbase, const std::string& username, const std::string& password) {
     if (userbase.userExists(username))
@@ -42,6 +45,45 @@ bool createUser(Userbase& userbase, const std::string& username, const std::stri
             bool ok = createUser(userbase, username, password);
             return crow::response(crow::json::wvalue(ok));
         });
-    
+
+	CROW_ROUTE(app, "/items")
+	([&market]()
+	{
+		crow::jason::wvalue results;
+		std::vector<Item> items = market.getItems();
+		for(int i = 0; i< items.size(); i++)
+		{	
+			result[i]["name"] = items[i].getName();
+			result[i]["description"] = items[i].getDescription();
+			result[i]["price"] = items[i].getBuyNowPrice();
+		}
+		return results;
+	});
+	CROW_ROUTE(app, "/additem")
+	.methods("POST"_method)
+	([&market](const crow::request& req) {
+		auto body = crow::json::load(req.body);
+
+		std::string name = body["name"].s();
+		std::string description = body["description"].s();
+		double price = body["price"].d();
+
+		market.addItem(name, description, price);
+
+		return crow::response("Item added");
+	});
+	CROW_ROUTE(app, "/bid")
+	.methods("POST"_method)
+	([&market](const crow::request& req) {
+		auto body = crow::json::load(req.body);
+
+		std::string itemName = body["item"].s();
+		std::string username = body["user"].s();
+		double amount = body["amount"].d();
+
+		bool success = market.placeBid(itemName, username, amount);
+		return crow::response(crow::json::wvalue(success));
+	});
+	std::cout<<"Servver Running On Port 18080\n";
         app.port(18080).multithreaded().run();
  }
