0c75dd4d11e5a2ef9ec61acf10fbf9da4e6cb742
Jesse Liu <2587118972@qq.com>
2026-03-07 17:18:48 -0800

    Update Userbase.cpp

--- backend/Userbase.cpp
+++ backend/Userbase.cpp
@@ -1,1 +1,23 @@
 #include "Userbase.h"
+
+Userbase::Userbase() {}
+
+void Userbase::addUser(const User& user)
+{
+    users[user.getUsername()] = user;
+}
+
+User* Userbase::getUser(const std::string& username)
+{
+    auto it = users.find(username);
+
+    if(it != users.end())
+        return &(it->second);
+
+    return nullptr;
+}
+
+bool Userbase::userExists(const std::string& username) const
+{
+    return users.find(username) != users.end();
+}
